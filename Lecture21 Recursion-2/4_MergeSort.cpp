6e43df7364d220f17f84d8adf5f1b362eda548de
prateekthakur <prateekthakur0711@gmail.com>
2026-02-19 16:53:02 +0530

    corrected merge sort

--- Lecture21 Recursion-2/4_MergeSort.cpp
+++ Lecture21 Recursion-2/4_MergeSort.cpp
@@ -2,65 +2,66 @@
 using namespace std;
 
 void merge(int *a, int *b, int *c, int s, int e){
-	int m = (s + e) / 2;
-	int i = s, j = m + 1, k = s;
-	while (i <= m and j <= e){
-		if (c[j] < b[i]){
-			a[k++] = c[j++];
-		}
-		else {
-			a[k++] = b[i++];
-		}
-	}
-	while (i <= m){
-		a[k++] = b[i++];
-	}
-	while (j <= e){
-		a[k++] = c[j++];
-	}
+    int m = (s + e) / 2;
+    int n1 = m - s + 1;
+    int n2 = e - m;
+
+    int i = 0, j = 0, k = s;
+
+    while (i < n1 && j < n2){
+        if (c[j] < b[i]){
+            a[k++] = c[j++];
+        }
+        else{
+            a[k++] = b[i++];
+        }
+    }
+
+    while (i < n1){
+        a[k++] = b[i++];
+    }
+
+    while (j < n2){
+        a[k++] = c[j++];
+    }
 }
 
 void mergeSort(int *a, int s, int e)
 {
-	// Base case
-	if (s == e){
-		return;
-	}
+    if (s >= e){
+        return;
+    }
 
-	// Recursive case
-	// 1. Divide
-	int m = (s + e) / 2;
-	int n1 = m - s + 1;
-	int n2 = e - m;
-	int b[n1], c[n2];
-	for (int i = s; i <= m; ++i)
-	{
-		b[i] = a[i];
-	}
-	for (int i = m + 1; i <= e; ++i)
-	{
-		c[i] = a[i];
-	}
+    int m = (s + e) / 2;
+    int n1 = m - s + 1;
+    int n2 = e - m;
 
-	// 2. Sort
-	mergeSort(b, s, m);
-	mergeSort(c, m + 1, e);
+    int b[n1], c[n2];
 
-	// 3. Merge
-	merge(a, b, c, s, e);
+    for (int i = 0; i < n1; i++){
+        b[i] = a[s + i];
+    }
+
+    for (int i = 0; i < n2; i++){
+        c[i] = a[m + 1 + i];
+    }
+
+    mergeSort(b, 0, n1 - 1);
+    mergeSort(c, 0, n2 - 1);
+
+    merge(a, b, c, s, e);
 }
 
 int main (){
+    int a[] = {6, 5, 7, 4, 3, 8, 9, 0, 1, 2, -11, 784 , -10000};
+    int n = sizeof(a) / sizeof(int);
 
-	int a[] = {6, 5, 7, 4, 3, 8, 9, 0, 1, 2, -11, 784 , -10000};
-	int n = sizeof(a) / sizeof(int);
     mergeSort(a, 0, n - 1);
 
-    for (int i = 0; i <= n;  ++i)
-    {
-    	cout << a[i] << " ";
+    for (int i = 0; i < n; ++i){
+        cout << a[i] << " ";
     }
-    cout << endl;
 
-	  return 0;
+    return 0;
 }
+
